9a7b6ff911e364f98f6eec1d3f96e67c15dc9666
suresh656 <32666465+suresh656@users.noreply.github.com>
2018-05-21 15:57:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -78,7 +78,7 @@ def posti():
                                                     respo = {"fulfillmentText": "Please enter a valid 10 digit mobile number","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {"name": "mobile"}}
                                                     return jsonify(respo)
                             elif req[header][item][option] == "askmobile - fallback":
-                                 respo = {"fulfillmentText": "Lead creation successful! Lead ID 149875 has been assigned to this SFE,he will contact you further. ","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                                 respo = {"fulfillmentText": "","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {"name": "mobile"}}
                                  return jsonify(respo)                    
                             elif req[header][item][option] == "askotp":
                                 for itm in req[header]:
@@ -94,7 +94,7 @@ def posti():
                                                     respo = {"fulfillmentText": "Please enter a valid 6 digits OTP sent to your mobile number","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
                                                     return jsonify(respo)
                             elif req[header][item][option] == "askotp - fallback":
-                                 respo = {"fulfillmentText": "Lead creation successful! Lead ID 149875 has been assigned to this SFE,he will contact you further. ","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                                 respo = {"","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {"name": "otp"}}
                                  return jsonify(respo)                    
                             elif req[header][item][option] == "Vehicleloan":
                                  respo = {"fulfillmentText": "Lead creation successful! Lead ID 149875 has been assigned to this SFE,he will contact you further. ","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
